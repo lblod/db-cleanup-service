af0a9012d2afb5a1697ed30cc8a44146d219d514
Riad Abdallah <riad.abdallah@hotmail.com>
2024-07-31 23:02:02 +0200

    Add endpoint for disabling all cronjobs

--- app.js
+++ app.js
@@ -15,19 +15,37 @@ cron.schedule(cronFrequency, function() {
 const performAllCleanups = async function() {
   const jobs = await CleanupJob.findAll();
   for (let job of jobs) {
+    console.log(`Creating cronjob with ID: ${job.id}`);
     scheduleCleanupJob(job);
   }
 };
 
-const isRunning = async function() {
-  return false;
-};
+const disableCronjobs = async function() {
+  const jobs = cron.getTasks();
+  for (const [jobId, job] of jobs) {
+    job.stop();
+    console.log(`Stopped cronjob with ID: ${jobId}`);
+  }
+}
 
 app.post('/cleanup/', async function( req, res, next ) {
   try {
-    if (await isRunning())
-      return res.status(503).end();
-    performAllCleanups();
+    await performAllCleanups();
+    return res.status(202).send({status: 202, title: 'processing'});
+  }
+  catch(e) {
+    console.error(e);
+    return next(new Error(e.message));
+  }
+});
+
+/*
+ * This is endpoint used to disable all scheduled cron jobs. It is needed
+ * in case the jobs need to be updated or
+ */
+app.post('/disableCronjobs', async function( req, res, next ) {
+  try {
+    await disableCronjobs();
     return res.status(202).send({status: 202, title: 'processing'});
   }
   catch(e) {
